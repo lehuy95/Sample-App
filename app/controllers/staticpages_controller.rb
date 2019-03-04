04e1604ced9adce513011832b7730f052edfde6d
Le Van Huy <huylevp95@gmail.com>
2019-03-04 19:54:40 +0700

    Finish layout and Routes

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -8,4 +8,8 @@ class StaticpagesController < ApplicationController
   def about
 
   end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'staticpages#home'
-  get 'staticpages/help'
-  get 'staticpages/about'
+  get '/help', to: 'staticpages#help'
+  get '/about', to: 'staticpages#about'
+  get '/contact', to: 'staticpages#contact'
+  get '/signup', to: 'users#new'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
